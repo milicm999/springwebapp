fd869974b307ae76ec97cb452298e827b2b7093a
Marija999 <105052720+milicm999@users.noreply.github.com>
2022-06-27 14:49:57 +0200

    AuthorController added

--- src/main/java/com/maja/springwebapp/SpringwebappApplication.java
+++ src/main/java/com/maja/springwebapp/SpringwebappApplication.java
@@ -1,13 +1,27 @@
 package com.maja.springwebapp;
 
+import com.maja.springwebapp.bootstrap.BootstrapData;
+import com.maja.springwebapp.controllers.BookController;
+import com.maja.springwebapp.entities.Author;
+import com.maja.springwebapp.entities.Book;
+import com.maja.springwebapp.entities.Publisher;
+import com.maja.springwebapp.repositories.AuthorRepository;
+import com.maja.springwebapp.repositories.BookRepository;
+import com.maja.springwebapp.repositories.PublisherRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Optional;
 
 @SpringBootApplication
+@ComponentScan
 public class SpringwebappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringwebappApplication.class, args);
+
+
 	}
 
 }
--- src/main/java/com/maja/springwebapp/controllers/BookController.java
+++ src/main/java/com/maja/springwebapp/controllers/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class BookController {
 
@@ -15,16 +16,11 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
-    @Autowired
-    public void setBookRepository(BookRepository bookRepository1)
-    {
-        bookRepository=bookRepository1;
-    }
 
     @RequestMapping("/books")
     public String getBooks(Model model)
     {
         model.addAttribute("books", bookRepository.findAll());
-        return "books/list";
+        return "books/listOfBooks";
     }
 }
